d329f575c73abbbd08de196fbaa49cf3176cd62c
Irfanul Amin <irfanulamin10@gmail.com>
2025-09-05 15:50:16 +0600

    cookie issue

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -24,7 +24,7 @@ export const register = async (
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
