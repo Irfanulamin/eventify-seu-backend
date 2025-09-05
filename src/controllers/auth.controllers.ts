b665119f4992f54f33b48622c3b3f56b3adfb8a8
Irfanul Amin <irfanulamin10@gmail.com>
2025-09-05 16:34:14 +0600

    cookie issues

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -25,8 +25,8 @@ export const register = async (
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     const userData = {
@@ -54,11 +54,10 @@ export const login = async (req: Request, res: Response): Promise<Response> => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
     const userData = {
       id: user._id,
       username: user.username,
